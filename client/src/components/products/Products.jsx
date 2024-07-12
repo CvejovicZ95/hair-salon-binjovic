befa4f62c6ce58452563f9f612c834afefbd6ead
CvejovicZ95 <cvejovicz@gmail.com>
2024-07-12 15:29:21 +0200

    added all products to database and listing them to front page

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -5,56 +5,56 @@ import { useGetProductsByCategory } from "../../hooks/useGetProducts";
 import "./Products.css";
 
 export const Products = () => {
+  // eslint-disable-next-line
   const [activeTab, setActiveTab] = useState('kerastase');
   const { productsByCategory, loading, error } = useGetProductsByCategory(activeTab);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+  const groupProductsByCategoryName = (products) => {
+    const groupedProducts = {};
+    products.forEach(product => {
+      const categoryName = product.name; 
+      if (!groupedProducts[categoryName]) {
+        groupedProducts[categoryName] = [];
+      }
+      groupedProducts[categoryName].push(product);
+    });
+    return groupedProducts;
   };
+
+  const groupedProducts = groupProductsByCategoryName(productsByCategory);
 
   return (
     <div className="products-page">
-      <Logo2/>
+      <Logo2 />
       <h1>PREPARATI</h1>
-      <div className="tab-buttons">
-        <button onClick={() => handleTabClick('kerastase')} className={activeTab === 'kerastase' ? 'active' : ''}>Kérastase</button>
-        <button onClick={() => handleTabClick('loreal')} className={activeTab === 'loreal' ? 'active' : ''}>L'Oréal</button>
-      </div>
+      <img
+        className="product-img"
+        src="kerastase.png"
+        alt="kerastase"
+      />
       <div className="products" id="products">
-        <div>
-          {activeTab === 'kerastase' && (
-            <img
-              className="product-img"
-              src="kerastase.png"
-              alt="kerastase"
-            />
-          )}
-          {activeTab === 'loreal' && (
-            <img
-              className="product-img"
-              src="loreal2.png"
-              alt="loreal"
-            />
-          )}
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error fetching products.</p>
+        ) : (
           <div className="products-list">
-            {loading ? (
-              <p>Loading...</p>
-            ) : error ? (
-              <p>Error fetching products.</p>
-            ) : (
-              productsByCategory.map(product => (
-                <div key={product._id} className="product-item">
-                  <h2>{product.name}</h2>
-                  <p>{product.preparate}</p>
-                  <p>Kolicina: {product.quantity}</p>
-                  <p>Cena: {product.price}</p>
-                </div>
-              ))
-            )}
+            {Object.keys(groupedProducts).map(categoryName => (
+              <div key={categoryName}>
+                <h2>{categoryName}</h2>
+                {groupedProducts[categoryName].map(product => (
+                  <div key={product._id} className="product-item">
+                    <p>{product.preparate}</p>
+                    <p>Količina: {product.quantity}</p>
+                    <p>Cena: {product.price}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
-        </div>
+        )}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };

7558bbc61d0f7ecbc131b76b751d7d7900b83fad
CvejovicZ95 <cvejovicz@gmail.com>
2024-08-15 15:09:36 +0200

    mailgun

--- server/mailgun.js
+++ server/mailgun.js
@@ -1,12 +1,14 @@
 import formData from 'form-data'
 import Mailgun from 'mailgun.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const mailgun = new Mailgun(formData)
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY })
 
 export const sendOrderConfirmation = async ({ email, details }) => {
   try {
-    const response = await mg.messages.create('sandbox5300715b146f476097b83e3211f67708.mailgun.org', {
+    const response = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
       from: 'Hair Salon Binjovic <mailgun@your-domain.com>',
       to: email,
       subject: 'Potvrda porudžbine',
